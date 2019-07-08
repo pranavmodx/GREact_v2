3f979c93e65033e2c7f1a829acbada24720ea35e
pranavmodx <pranavmodx@gmail.com>
2019-07-09 04:41:35 +0530

    prettified json data in fixture

--- backend/main/fixtures/json_formatter.py
+++ backend/main/fixtures/json_formatter.py
@@ -3,19 +3,7 @@ import json
 with open('backend/main/fixtures/gredata.json', 'r') as f:
     data = json.load(f)
 
-# # print(data[0])
-# sample = data[0]
-
-# new_dict = {
-#     "pk": f'1',
-#     "model": 'sessions.session',
-#     "fields": sample
-# }
-
-# # print(new_dict)
-# print(len(data))
-
-with open('backend/main/fixtures/gredata3.json', 'w') as f:
+with open('backend/main/fixtures/data.json', 'w') as f:
     rlist = []
     for i in range(1, len(data)+1):
         for k,v in data[i-1].items():
